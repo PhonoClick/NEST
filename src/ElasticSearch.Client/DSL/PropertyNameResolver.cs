8909f50777737b5856e4f69b6b656da6b9c0c1f9
kerem <kerem1609@phonoclick.com>
2017-10-19 15:45:22 +0300

    newtonsoftJson and log4net upgrade

--- src/ElasticSearch.Client/DSL/PropertyNameResolver.cs
+++ src/ElasticSearch.Client/DSL/PropertyNameResolver.cs
@@ -1,39 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.Linq.Expressions;
-using System.Reflection;
-using Fasterflect;
 
 namespace ElasticSearch.Client.DSL
 {
-	class PropertyNameResolver
+  class PropertyNameResolver
 	{
 		private JsonSerializerSettings SerializationSettings { get; set; }
 		private IContractResolver ContractResolver { get; set; }
-		private MethodInvoker PropertyResolver { get; set; }
+		//private MethodInvoker PropertyResolver { get; set; }
 
 		public PropertyNameResolver(JsonSerializerSettings settings)
 		{
 			this.SerializationSettings = settings;
 			this.ContractResolver = settings.ContractResolver;
 
-			var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly;
-			this.PropertyResolver = this.ContractResolver.GetType()
-				.DelegateForCallMethod("ResolvePropertyName", bindingFlags , new[] { typeof(string) });
+      // NOTE: json.net yeni surumlerinde camelcasecontracter classindan ResolvePropertyName fonksiyonu kaldirmis. oradak tek yaptigi stringutils.tocamelcase'i cagirmakti. onu budan manuel biz yapiyoruz artik.
+      //var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly;
+      //this.PropertyResolver = this.ContractResolver.GetType().DelegateForCallMethod("ResolvePropertyName", bindingFlags, new[] { typeof(string) });
 
-		}
-		public string Resolve(MemberExpression memberExpression)
+    }
+    public string Resolve(MemberExpression memberExpression)
 		{
 			var segments = new List<string>();
 			while (memberExpression != null)
 			{
 				var name = memberExpression.Member.Name;
-				var resolvedName = this.PropertyResolver(this.ContractResolver, name) as string;
-				segments.Insert(0, resolvedName);
+        //var resolvedName = this.PropertyResolver(this.ContractResolver, name) as string;
+        var resolvedName = StringUtils.ToCamelCase(name);
+
+        segments.Insert(0, resolvedName);
 				memberExpression = memberExpression.Expression as MemberExpression;
 			}
 			return string.Join(".",segments.ToArray());
